be7afc4a6f2725506cf8151029cfda0da1f6abb0
vakiko <victoria.c6140@gmail.com>
2023-01-28 15:38:01 -0600

    preferences - add seating & confirm button

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -6,7 +6,11 @@ import preference from "../preference";
 const Preferences: NextPage = () => {
 
   return (
-    <div className="flex flex-col justify-start items-start">
+    <div className="flex flex-col justify-start items-start p-10">
+      <p>First, we need some information.</p>
+      <br></br>
+      <div className="bg-slate-100">
+      <p>Please select which of the following apply to you:</p>
       <form>
         <input type="checkbox" value="Veteran" />
         <label> Veteran</label><br />
@@ -15,6 +19,24 @@ const Preferences: NextPage = () => {
         <input type="checkbox" value="Elderly" />
         <label> Elderly</label><br /><br />
       </form>
+      </div>
+      <div className="bg-slate-100">
+      <p>Please select your preferred seat(s):</p>
+      <form>
+        <input type="checkbox" value="Aisle" />
+        <label> Aisle</label><br />
+        <input type="checkbox" value="Middle" />
+        <label> Middle</label><br />
+        <input type="checkbox" value="Window" />
+        <label> Window</label><br /><br />
+      </form>
+      </div>
+      <Link href="/ticketInput">
+          <button className="w-32 bg-zinc-200 py-1 px-2 rounded-xl">
+            <p>Confirm Choice</p>
+          </button>
+        </Link>
+      <p>Note: You are not guaranteed your choice of seating, but we will do our best to match you. </p>
     </div>
   );
 };
