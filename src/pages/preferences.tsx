c3950c70e5df50e9cd1031b429abfa83dca8f171
Adam Teo <adam.teo321@gmail.com>
2023-01-28 16:08:56 -0600

    Change pref

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -7,35 +7,37 @@ const Preferences: NextPage = () => {
 
   return (
     <div className="flex flex-col justify-start items-start p-10">
-      <p>First, we need some information.</p>
+      <div className="flex text-xl font-semibold">
+        <p>First, we need some information.</p>
+      </div>
       <br></br>
       <div className="bg-slate-100">
-      <p>Please select which of the following apply to you:</p>
-      <form>
-        <input type="checkbox" value="Veteran" />
-        <label> Veteran</label><br />
-        <input type="checkbox" value="Disabled" />
-        <label> Disabled</label><br />
-        <input type="checkbox" value="Elderly" />
-        <label> Elderly</label><br /><br />
-      </form>
+        <p>Please select which of the following apply to you:</p>
+        <form>
+          <input type="checkbox" value="Veteran" />
+          <label> Veteran</label><br />
+          <input type="checkbox" value="Disabled" />
+          <label> Disabled</label><br />
+          <input type="checkbox" value="Elderly" />
+          <label> Elderly</label><br /><br />
+        </form>
       </div>
       <div className="bg-slate-100">
-      <p>Please select your preferred seat(s):</p>
-      <form>
-        <input type="checkbox" value="Aisle" />
-        <label> Aisle</label><br />
-        <input type="checkbox" value="Middle" />
-        <label> Middle</label><br />
-        <input type="checkbox" value="Window" />
-        <label> Window</label><br /><br />
-      </form>
+        <p>Please select your preferred seat(s):</p>
+        <form>
+          <input type="checkbox" value="Aisle" />
+          <label> Aisle</label><br />
+          <input type="checkbox" value="Middle" />
+          <label> Middle</label><br />
+          <input type="checkbox" value="Window" />
+          <label> Window</label><br /><br />
+        </form>
       </div>
       <Link href="/ticketInput">
-          <button className="w-32 bg-zinc-200 py-1 px-2 rounded-xl">
-            <p>Confirm Choice</p>
-          </button>
-        </Link>
+        <button className="w-32 bg-zinc-200 py-1 px-2 rounded-xl">
+          <p>Confirm Choice</p>
+        </button>
+      </Link>
       <p>Note: You are not guaranteed your choice of seating, but we will do our best to match you. </p>
     </div>
   );
--- src/pages/ticketInput.tsx
+++ src/pages/ticketInput.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const ticketInput = () => {
@@ -5,19 +6,23 @@ const ticketInput = () => {
     <div className="flex flex-col justify-center align-center h-screen w-full gap-8">
       <div className="gap-2">
         <form className="flex flex-col items-center justify-center">
-          <label className="flex flex-col items-center justify-center w-full">
+          <label className="flex flex-col items-center justify-center w-full font-semibold">
             Ticket Number:
-            <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="ticketNum" placeholder="ex. 001234567890" />
+            <input className="mt-2 bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 dark:bg-zinc-300 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="ticketNum" placeholder="ex. 001234567890" />
           </label>
         </form>
       </div>
-      <div className="flex flex-row items-center justify-center w-full gap-4">
-        <button className="w-32 bg-zinc-100 py-1 px-2 rounded-xl" >
-          <p>Specific Seat</p>
-        </button>
-        <button className="w-32 bg-zinc-100 py-1 px-2 rounded-xl">
-          <p>Preferences</p>
-        </button>
+      <div className="flex flex-row items-center justify-center w-full gap-4 font-semibold">
+        <Link href="/seatSelect">
+          <button className="w-32 bg-zinc-100 py-1 px-2 rounded-xl" >
+            <p>Specific Seat</p>
+          </button>
+        </Link>
+        <Link href="/preferences">
+          <button className="w-32 bg-zinc-100 py-1 px-2 rounded-xl">
+            <p>Preferences</p>
+          </button>
+        </Link>
       </div>
     </div>
   )
