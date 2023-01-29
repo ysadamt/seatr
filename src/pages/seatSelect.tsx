11beac07c0f95cd6bc5710e78bdf05295f432ccc
Adam Teo <adam.teo321@gmail.com>
2023-01-28 20:16:04 -0600

    Start seat page

--- src/pages/seatSelect.tsx
+++ src/pages/seatSelect.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const seatSelect = () => {
   return (
-    <div>seatSelect</div>
+
+    <div className="flex flex-col justify-start items-center p-8 md:justify-center md:items-center bg-gradient-to-b from-sky-300 to-sky-400">
+      <h1 className="font-semibold text-xl text-white my-4">Please Choose your Seat</h1>
+      <button className="w-43 p-3 rounded-xl bg-[#195770] text-white font-semibold hover:bg-[#154153] my-4">
+          <p>Confirm Choice</p>
+        </button>
+      <div className="">
+        {/* seat map */}
+      </div>
+    </div>
   )
 }
 
--- src/pages/ticketInput.tsx
+++ src/pages/ticketInput.tsx
@@ -3,32 +3,50 @@ import React, { useState, useRef, useEffect } from "react";
 
 const ticketInput = () => {
   const [ticketNum, setTicketNum] = useState("");
+  const [valid, setValid] = useState(true);
 
-  useEffect(() => {
-    console.log(ticketNum);
-  }, [ticketNum]);
-  
+  const handleSpecificSubmit = (e: any) => {
+    e.preventDefault();
+    if (ticketNum.length === 12) {
+      window.location.href = "/seatSelect";
+    } else {
+      setValid(false);
+    }
+  };
+
+  const handlePrefSubmit = (e: any) => {
+    e.preventDefault();
+    if (ticketNum.length === 12) {
+      window.location.href = "/preferences";
+    } else {
+      setValid(false);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center align-center h-screen w-full gap-8 bg-gradient-to-b from-sky-300 to-sky-400">
       <div className="gap-2">
         <form className="flex flex-col items-center justify-center" >
           <label className="flex flex-col items-center justify-center w-full font-semibold text-xl text-[#195770]">
             Ticket Number:
-            <input className="mt-2 bg-white border-2 border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5" type="text" name="ticketNum" placeholder="ex. 001234567890" onChange={e => setTicketNum(e.target.value)} />
+            <input className="mt-2 bg-white border-2 border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 shadow-lg" type="text" name="ticketNum" placeholder="ex. 001234567890" onChange={e => setTicketNum(e.target.value)} />
           </label>
         </form>
       </div>
-      <div className="flex flex-row items-center justify-center w-full gap-4 font-semibold">
-        <Link href="/seatSelect">
-          <button className="w-32 p-2 rounded-xl bg-[#195770]">
+      <div className="flex flex-row items-center justify-center w-full gap-8 font-semibold">
+        <form onSubmit={handleSpecificSubmit}>
+          <button className="w-32 p-2 rounded-xl bg-[#195770] shadow-lg" type="submit">
             <p className="text-white">Specific Seat</p>
           </button>
-        </Link>
-        <Link href="/preferences">
-          <button className="w-32 p-2 rounded-xl bg-[#195770]">
+        </form>
+        <form onSubmit={handlePrefSubmit}>
+          <button className="w-32 p-2 rounded-xl bg-[#195770] shadow-lg">
             <p className="text-white">Preferences</p>
           </button>
-        </Link>
+        </form>
+      </div>
+      <div className="absolute m-auto left-0 right-0 bottom-60 text-center">
+        {!valid && <p className="text-red-500 font-semibold italic">Invalid Ticket Number</p>}
       </div>
     </div>
   )
