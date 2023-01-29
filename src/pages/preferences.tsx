ef265d38b126fb47925510101e53a9d1efa6818e
vakiko <victoria.c6140@gmail.com>
2023-01-28 18:18:05 -0600

    add hover & fullscreen md & lg support

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -6,7 +6,7 @@ import preference from "../preference";
 const Preferences: NextPage = () => {
 
   return (
-    <div className="flex flex-col justify-start items-start p-8 md:justify-center md:items-center bg-gradient-to-b from-sky-300 to-sky-400">
+    <div className="flex flex-col justify-start items-start p-8 md:justify-center md:items-center bg-gradient-to-b from-sky-300 to-sky-400 md:min-h-screen">
       <div className="flex text-xl font-semibold text-white">
         <p className="text-[#195770]">First, we need some information.</p>
       </div>
