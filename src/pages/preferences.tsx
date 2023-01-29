f1b52a8ba19ed86edabf980eb018595b5706899d
vakiko <victoria.c6140@gmail.com>
2023-01-28 19:48:38 -0600

    remove daisyui

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import preference from "../preference";
 
 const Preferences: NextPage = () => {
 
@@ -11,7 +10,7 @@ const Preferences: NextPage = () => {
         <p className="text-[#195770]">First, we need some information.</p>
       </div>
       <br></br>
-      <div className="bg-white p-6 rounded-lg hover:bg-slate-100">
+      <div className="bg-white p-6 rounded-lg hover:bg-slate-100 shadow-lg">
         <p>Please select which of the following apply to you:</p>
         <form>
           <input type="checkbox" value="Veteran" />
@@ -25,7 +24,7 @@ const Preferences: NextPage = () => {
         </form>
       </div>
       <div className="p-2"></div>
-      <div className="bg-white p-6 rounded-lg hover:bg-slate-100">
+      <div className="bg-white p-6 rounded-lg hover:bg-slate-100 shadow-lg">
         <p>Please select your preferred seat(s):</p>
         <form>
           <input type="checkbox" value="Aisle" />
@@ -37,7 +36,7 @@ const Preferences: NextPage = () => {
         </form>
       </div>
       <Link className="pt-6 pb-5" href="/ticketInput">
-        <button className="w-43 p-3 rounded-xl bg-[#195770] text-white font-semibold">
+        <button className="w-43 p-3 rounded-xl bg-[#195770] text-white font-semibold hover:bg-[#154153]">
           <p>Confirm Choice</p>
         </button>
       </Link>
--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const result = () => {
   return (
-    <div>result</div>
-  )
+    <>
+    <div className="flex flex-col justify-start items-start p-8 md:justify-center md:items-center bg-gradient-to-b from-sky-300 to-sky-400 md:min-h-screen">
+    
+   </div>
+    </>
+  );
 }
 
 export default result
