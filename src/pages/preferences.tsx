40188abc447399025b45abd49880dded39f04573
vakiko <victoria.c6140@gmail.com>
2023-01-28 18:12:36 -0600

    consistent preferences styling

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -8,10 +8,10 @@ const Preferences: NextPage = () => {
   return (
     <div className="flex flex-col justify-start items-start p-8 md:justify-center md:items-center bg-gradient-to-b from-sky-300 to-sky-400">
       <div className="flex text-xl font-semibold text-white">
-        <p className="text-white">First, we need some information.</p>
+        <p className="text-[#195770]">First, we need some information.</p>
       </div>
       <br></br>
-      <div className="bg-white p-5 rounded-lg hover:bg-slate-100">
+      <div className="bg-white p-6 rounded-lg hover:bg-slate-100">
         <p>Please select which of the following apply to you:</p>
         <form>
           <input type="checkbox" value="Veteran" />
@@ -25,7 +25,7 @@ const Preferences: NextPage = () => {
         </form>
       </div>
       <div className="p-2"></div>
-      <div className="bg-white p-5 rounded-lg hover:bg-slate-100">
+      <div className="bg-white p-6 rounded-lg hover:bg-slate-100">
         <p>Please select your preferred seat(s):</p>
         <form>
           <input type="checkbox" value="Aisle" />
@@ -37,11 +37,11 @@ const Preferences: NextPage = () => {
         </form>
       </div>
       <Link className="pt-6 pb-5" href="/ticketInput">
-        <button className="w-50 bg-white p-2 rounded-xl">
+        <button className="w-43 p-3 rounded-xl bg-[#195770] text-white font-semibold">
           <p>Confirm Choice</p>
         </button>
       </Link>
-      <div className="text-white">
+      <div className="text-[#195770]">
       <p className="pb-5">Note: You are not guaranteed your choice of seating, but we will do our best to match you. </p>
       </div>
     </div>
