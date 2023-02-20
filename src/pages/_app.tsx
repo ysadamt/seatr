264405166d9a41671541710a1414a10c02f0ac38
Adam Teo <adam.teo321@gmail.com>
2023-02-19 20:35:10 -0600

    Remove sqlite files and add page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
 import "../styles/globals.css";
 import '../styles/clouds.css';
 import '../styles/animation.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Seatr</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="../favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
